4ef69180fd9977204bed6bbe0e5099c55d4f0d67
Juan Camilo Gonzalez <info@juancgonzalez.com>
2014-12-22 19:50:22 +0800

    clean header

--- header.php
+++ header.php
@@ -1,57 +1,39 @@
 <!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
 
-<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
-<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
-<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+    <meta name="msapplication-TileColor" content="#5698c1">
+    <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+    <meta name="theme-color" content="#3f647b">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <?php wp_head(); ?>
+  </head>
 
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="HandheldFriendly" content="True">
-  <meta name="MobileOptimized" content="320">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
-  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-  <!--[if IE]>
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-  <![endif]-->
-  <?php // or, set /favicon.ico for IE10 win ?>
-  <meta name="msapplication-TileColor" content="#f01d4f">
-  <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
-  <meta name="theme-color" content="#121212">
+  <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 
-  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-
-  <?php wp_head(); ?>
-
-</head>
-
-<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
-
-  <div id="container">
-
-    <header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
-
-      <p id="site-name" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
-
-      <nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-        <?php
-        wp_nav_menu(array(
-          'container'       => false,
-          'container_class' => 'menu cf',
-          'menu'            => 'The Main Menu',
-          'menu_class'      => 'nav top-nav cf',
-          'theme_location'  => 'main-nav',
-          'before'          => '',
-          'after'           => '',
-          'link_before'     => '',
-          'link_after'      => '',
-          'depth'           => 0,
-          'fallback_cb'     => ''
-        ));
-        ?>
-
-      </nav>
-
-    </header>
+    <div id="container">
+      <header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+        <p id="site-name" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+        <nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+          <?php
+          wp_nav_menu(array(
+            'container'       => false,
+            'container_class' => 'menu cf',
+            'menu'            => 'The Main Menu',
+            'menu_class'      => 'nav top-nav cf',
+            'theme_location'  => 'main-nav',
+            'before'          => '',
+            'after'           => '',
+            'link_before'     => '',
+            'link_after'      => '',
+            'depth'           => 0,
+            'fallback_cb'     => ''
+          ));
+          ?>
+        </nav>
+      </header>
