c8c50b9ab98c6bf5e64a159704f7eb502f0b6fd8
Juan Camilo Gonzalez <info@juancgonzalez.com>
2014-12-23 23:14:23 +0800

    fix sidebar display

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,5 @@
-<div id="main-sidebar" class="sidebar m-100 t-100 d-20 ld-30 last-col cf" role="complementary">
-
-  <?php
-  if ( is_active_sidebar('main-sidebar') ) {
-    dynamic_sidebar( 'main-sidebar' );
-  }
-  ?>
-
-</div>
+<?php if ( is_active_sidebar('main-sidebar') ) : ?>
+  <div id="main-sidebar" class="sidebar m-100 t-100 d-20 ld-30 last-col cf" role="complementary">
+    <?php dynamic_sidebar( 'main-sidebar' ); ?>
+  </div>
+<?php endif; ?>
