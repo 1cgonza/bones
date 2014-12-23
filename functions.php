a5db012b3d217e35f7124a55074e27e68e1168de
Juan Camilo Gonzalez <info@juancgonzalez.com>
2014-12-23 23:13:46 +0800

    widgets class name

--- functions.php
+++ functions.php
@@ -48,11 +48,10 @@ function jcg_register_sidebars() {
     'description'   => 'The main sidebar.',
     'before_widget' => '<div id="%1$s" class="widget %2$s">',
     'after_widget'  => '</div>',
-    'before_title'  => '<h4 class="widgettitle">',
+    'before_title'  => '<h4 class="widget-title">',
     'after_title'   => '</h4>',
   ));
 }
-
 
 /*======================================
 =            COMMENT LAYOUT            =
